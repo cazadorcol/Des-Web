db1cf8c690fe52dd77dbbf28a144c89016ca7e1d
SALA DE CÓMPUTO <SALA@EAN3377.universidad.ean>
2019-04-04 07:13:00 -0500

    Commit 1

---
+++ JS/main.js
@@ -0,0 +1,29 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+
+    mainNav.classList.toggle('active');
+});
+
+function validacion() {
+    var nombreUsuario = document.getElementById("name").value;
+    var contraseñaUsuario = document.getElementById("contraseña").value;
+    var correoUsuario = document.getElementById("correo").value;
+    var caracteresNombre = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+    var caracteresCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      if (nombreUsuario.length === 0 || /^\s+$/.test(nombreUsuario)) {
+        alert('El campo de nombre esta vacio!');
+      }else if (nombreUsuario.length > 0 && nombreUsuario.match(caracteresNombre) === null) {
+        alert('El nombre debe tener solo letras');
+      }else if (correoUsuario.length === 0 || /^\s+$/.test(correoUsuario)) {
+        alert('El campo de correo esta vacio!');
+      } else if (correoUsuario.length > 0 && correoUsuario.match(caracteresCorreo) === null) {
+        alert('El campo del correo debe ser un correo valido!')
+      }else if (contraseñaUsuario.length == 0 || /^\s+$/.test(contraseñaUsuario)) {
+        alert('El campo de contraseña esta vacio!');
+      } else if (contraseñaUsuario.length > 0 && contraseñaUsuario.length < 8) {
+        alert('El campo de contraseña debe contener minimo 8 caracteres!')
+      }
+      return true;
+    }
