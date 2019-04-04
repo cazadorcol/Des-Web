32d4f1ce7d2154d930b07085cf7a21561933e7d9
SALA DE CÓMPUTO <SALA@EAN3377.universidad.ean>
2019-04-04 07:40:42 -0500

    commit cuadrar boton

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,3 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-
-    mainNav.classList.toggle('active');
-});
 
 function validacion() {
     var nombreUsuario = document.getElementById("name").value;
